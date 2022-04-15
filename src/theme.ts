f2f29b9e92a5cc7de1aecb4b8976be3c2b40a41e
flex-ar <jmarnedo8@gmail.com>
2022-04-15 20:30:40 -0300

    added customs themes

---
+++ src/theme.ts
@@ -0,0 +1,17 @@
+import { extendTheme } from '@chakra-ui/react';
+
+export const theme = extendTheme({
+  colors: {
+    bg: {
+      1: "#1E213A",
+      2: "#100E1D",
+      3: "#585676",
+      4: "#FFEC65"
+    },
+    text: {
+      1: "#E7E7EB",
+      2: "#A09FB1",
+      3: "#110E3C"
+    }
+  }
+})
